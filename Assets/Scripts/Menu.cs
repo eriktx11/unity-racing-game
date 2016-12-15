a1ee72897183036aae1e3db9b5433ec3165c7f96
eriktx11 <erik@llerenapiano.com>
2016-12-14 22:25:20 -0600

    Feat: FPS Menu
    Added scripts

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	private Animator _animator;
+	private CanvasGroup _canvasGroup;
+
+	public bool isOpen
+	{
+		get{return _animator.GetBool ("isOpen");}
+		set{_animator.SetBool ("isOpen", value);}
+	}
+
+	public void Awake(){
+		_animator = GetComponent<Animator>();
+		_canvasGroup = GetComponent<CanvasGroup>();
+
+		var rect = GetComponent<RectTransform> ();
+		rect.offsetMax = rect.offsetMin - new Vector2 (0,0);
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+	
+		if(!_animator.GetCurrentAnimatorStateInfo(0).IsName("Open")){
+			_canvasGroup.blocksRaycasts = _canvasGroup.interactable = false;
+		}else{
+			_canvasGroup.blocksRaycasts = _canvasGroup.interactable = true;
+		}
+	}
+}
---
+++ Assets/Scripts/MenuMngr.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMngr : MonoBehaviour {
+
+	public Menu currentMenu;
+
+	public void Start(){
+	
+		ShowMenu (currentMenu);
+	}
+
+	public void ShowMenu(Menu menu){
+	
+		if(currentMenu!=null){
+			currentMenu.isOpen = false;
+
+
+			currentMenu = menu;
+			currentMenu.isOpen = true;
+		}
+	}
+
+}
